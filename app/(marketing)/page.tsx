9f394a228dc162e284669818d7f75f83abf2a57f
El-Gazoly <elgazoly11@gmail.com>
2024-11-18 00:59:24 +0200

    fix: bg color in landing

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import Heros from "./_components/Heros";
 
 export default function MarketingPage() {
   return (
-    <main className="min-h-full flex flex-col ">
+    <main className="min-h-full flex flex-col dark:bg-dark-bg ">
       <div className=" flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heros />
