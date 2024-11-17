dc6fe57131ba963fe3ee958062545a0cce99e6ef
El-Gazoly <elgazoly11@gmail.com>
2024-11-17 20:27:17 +0200

    feature: update or delete a cover image

--- hooks/use-cover-image.tsx
+++ hooks/use-cover-image.tsx
@@ -11,7 +11,7 @@ type CoverImageStates = {
 export const useCoverImage = create<CoverImageStates>((set) => ({
   url: undefined,
   isOpen: false,
-  onOpen: () => set({ isOpen: true }),
+  onOpen: () => set({ isOpen: true, url: undefined }),
   onClose: () => set({ isOpen: false, url: undefined }),
   onReplace: (url) => set({ isOpen: true, url }),
 }));
